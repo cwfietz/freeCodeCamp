f87d71e2dc1054d780f7e0a2f010b7982744f261
cwfietz <cwfietz@calcna.ab.ca>
2021-06-22 00:00:30 -0600

    F skip counting in loops

---
+++ learn/javascript-algorithms-and-data-structures/basic-javascript/Loops/count-backwards-with-a-for-loop.js
@@ -0,0 +1,9 @@
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var increment = 9; increment>=1; increment -= 2) {
+    myArray.push(increment);
+}
+
+console.log(myArray);
---
+++ learn/javascript-algorithms-and-data-structures/basic-javascript/Loops/iterate-through-an-array-with-a-for-loop.js
@@ -0,0 +1,12 @@
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0
+for (var index = 0; index < myArr.length; index++) {
+    // console.log(myArr[index]);
+    total += myArr[index];
+    // console.log(total);
+}
+
+console.log("Total= " + total);
