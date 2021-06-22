591404361659ebf578d98f6e86a57eb3dc6d59b9
cwfietz <cwfietz@calcna.ab.ca>
2021-06-21 23:54:10 -0600

    F while oops

---
+++ learn/javascript-algorithms-and-data-structures/basic-javascript/Loops/iterate-with-javascript-while-loops.js
@@ -0,0 +1,10 @@
+// Setup
+var myArray = [];
+
+// Only change code below this line
+var interation = 0;
+while(interation <= 5) {
+    myArray.unshift(interation);
+    interation++;
+}
+console.log(myArray);
